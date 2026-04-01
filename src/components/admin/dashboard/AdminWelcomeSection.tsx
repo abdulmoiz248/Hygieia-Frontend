329304f506cf5103d610672ef5bddd8cdd0997d8
LaibaMaqsood91 <alishbahmaqsood09@gmail.com>
2026-04-01 20:42:36 +0500

    Modified Admin Dashboard Welcome Section

--- src/components/admin/dashboard/AdminWelcomeSection.tsx
+++ src/components/admin/dashboard/AdminWelcomeSection.tsx
@@ -33,7 +33,7 @@ export default function AdminWelcomeSection() {
       <SplitText
         text={
           <span>
-            <span className="text-soft-coral mt-4">Welcome, </span>
+            <span className="text-soft-coral mt-4">Welcome,&nbsp;</span>
             <span className="text-dark-slate-gray"> {adminName}! 👋</span>
           </span>
         }
